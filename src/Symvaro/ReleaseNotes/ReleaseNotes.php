163dc77ed9b0ab3e13a965abef3bce0fa4e41286
Daniel Baeck <db@sharpsrc.net>
2017-04-25 14:03:24 +0200

    Refactor and add note filtering

--- src/Symvaro/ReleaseNotes/ReleaseNotes.php
+++ src/Symvaro/ReleaseNotes/ReleaseNotes.php
@@ -3,18 +3,34 @@ namespace Symvaro\ReleaseNotes;
 
 class ReleaseNotes
 {
-	public function notesForLocale($locale = 'de')
+	public function notesForLocale($locale = 'de', $minDate = null, $limit = null)
 	{
-		//Release Notes - gather and sort and take the last one
+		//Release Notes - gather and sort
         $notes = \File::files(base_path(config('release-notes.path', 'resources/views/releases/') . $locale));
-        $notes = array_map(function ($file) {
-            return str_replace('.md', '', basename($file));
+        $filteredNotes = collect([]);
+        
+        $notes = array_map(function ($file) use ($filteredNotes) {
+        	$date = \DateTime::createFromFormat('Y_m_d_His', substr(basename($file), 0, 17));
+        	$obj = new \stdClass();
+        	$obj->date = $date;
+        	$obj->file = str_replace('.md', '', basename($file));
+        	$filteredNotes->push($obj);
 
         }, $notes);
 
-        sort($notes);
+        $filteredNotes = $filteredNotes->sortBy('date');
 
-        return $notes;
+        if (!is_null($minDate)) {
+        	$filteredNotes = $filteredNotes->filter(function ($item) use ($date) {
+			    return (data_get($item, 'date') > $date);
+			});
+        }		
+
+        if (!is_null($limit)) {
+        	$filteredNotes = $filteredNotes->take($limit);
+        }
+
+        return $filteredNotes;
 	}
 
 
@@ -34,33 +50,8 @@ class ReleaseNotes
 		return $this->lastNoteForLocale(\Lang::locale());
 	}
 
-	public function notesForCurrentLocale($since = null)
+	public function notesForCurrentLocale($since = null, $limit = null)
 	{
-		if (is_null($since)) {
-			return $this->notesForLocale(\Lang::locale());	
-		} else {
-			return $this->notesSince($since, \Lang::locale())->toArray();
-		}
-	}
-
-	public function notesSince($date, $locale = 'de')
-	{
-		$notes = \File::files(base_path(config('release-notes.path', 'resources/views/releases/') . $locale));
-		$filteredNotes = collect([]);
-
-		$notes = array_map(function ($file) use ($filteredNotes) {
-        	$date = \DateTime::createFromFormat('Y_m_d_His', substr(basename($file), 0, 17));
-        	$obj = new \stdClass();
-        	$obj->date = $date;
-        	$obj->file = str_replace('.md', '', basename($file));
-        	$filteredNotes->push($obj);
-
-        }, $notes);
-
-		$filteredNotes = $filteredNotes->filter(function ($item) use ($date) {
-		    return (data_get($item, 'date') > $date);
-		});
-
-		return $filteredNotes->pluck('file');
+		return $this->notesForLocale(\Lang::locale(), $since, $limit);	
 	}
 }
